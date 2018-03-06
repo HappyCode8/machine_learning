554a1883f1a4f563547aee25fe7bf4d872e5703d
github20131983 <32866135+github20131983@users.noreply.github.com>
2018-03-06 11:27:25 +0800

    Initial commit

---
+++ Ch2/KNN.py
@@ -0,0 +1,14 @@
+__author__ = 'wyj'
+
+from numpy import *
+import operator
+
+def creatDataSet():
+    group=array([[1.0,1.1],[1.0,1.0],[0,0],[0,0.1]]);
+    label=['A','A','B','B'];
+    return group,label;
+
+if __name__=="__main__":
+    group,label=creatDataSet();
+    print group;
+    print label;
