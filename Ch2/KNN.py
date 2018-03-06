18dc95e56dc118be8f9670b880b6668bcbde1c2f
github20131983 <32866135+github20131983@users.noreply.github.com>
2018-03-06 12:46:28 +0800

    12

--- Ch2/KNN.py
+++ Ch2/KNN.py
@@ -7,7 +7,6 @@ def creatDataSet():
     group=array([[1.0,1.1],[1.0,1.0],[0,0],[0,0.1]]);
     label=['A','A','B','B'];
     return group,label;
-
 if __name__=="__main__":
     group,label=creatDataSet();
     print group;
